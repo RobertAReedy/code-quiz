bd8dd3c42e134e36f991e9a2e8d8515e84e35fcf
RobertAReedy <robertandreedy@gmail.com>
2021-09-20 19:05:07 -0400

    question sheet removal and addition functions added, intro sheet styling in process

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,22 +7,42 @@ of the program, and will not ever be removed*/
 
 var mainEl = document.querySelector("#main-page");
 
+//The intro sheet
+var introSheetEl = document.createElement("div");
+introSheetEl.className = "intro-sheet";
+
 //These are the variables that need to be updated
 //on the question page
 var qSheetEl = document.createElement("div");
+qSheetEl.className = "question-sheet";
 var questionEl = document.createElement("h1");
 var answerButtons = [];
 var isCorrectEl = document.createElement("h2");
 
 /**
- * Will show the sheet used for asking questions. Currently
+ * Will create the sheet shown to the user first.
+ */
+var createIntroSheet = function() {
+    var introTitleEl = document.createElement("h1");
+    var introLineEl = document.createElement("h2");
+    var startButtonEl = document.createElement("button");
+    introTitleEl.textContent = "Coding Quiz Challenge";
+    introLineEl.textContent = "Try to answer the following code related questions within the time limit. Keep in mind that incorrect answers will penalize your score/time by ten seconds!";
+    startButtonEl.textContent = "Start Quiz";
+
+
+    introSheetEl.appendChild(introTitleEl);
+    introSheetEl.appendChild(introLineEl);
+    introSheetEl.appendChild(startButtonEl);
+}
+
+/**
+ * Will create the sheet used for asking questions. Currently
  * in styling and HTML stage.
  */
 var createQuestionSheet = function() {
     var questionAnswerBoxEl = document.createElement("div");
  
-
-    qSheetEl.className = "question-sheet";
     questionEl.textContent = "What was the birthplace of Mesopotamian civilization?";
     isCorrectEl.textContent = "will be correct or incorrect";
 
@@ -40,5 +60,20 @@ function showQuestionSheet() {
     mainEl.appendChild(qSheetEl);
 }
 
-createQuestionSheet();
+function hideQuestionSheet() {
+    mainEl.removeChild(qSheetEl);
+}
+
+function showIntroSheet() {
+    mainEl.appendChild(introSheetEl);
+}
+
+function hideIntroSheet() {
+    mainEl.removeChild(introSheetEl);
+}
+
+createQuestionSheet();  
+createIntroSheet(); 
+showIntroSheet(); 
+
 
