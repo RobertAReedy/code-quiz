f0f8d20c467334efab5c350f83c5e2c920060358
RobertAReedy <robertandreedy@gmail.com>
2021-09-24 21:01:36 -0400

    all functionality mostly complete, finishing touches remain

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,3 @@
-/* I should only make the header stuff transparent
-when viewing high scores, rather than disappearing
-them entirely*/
-
 /* The div called Main will stay for all states
 of the program, and will not ever be removed*/
 var mainEl = document.querySelector("#main-page");
@@ -21,6 +17,7 @@ var isCorrectEl = document.createElement("h2");
 //where the questions will be stored as objects
 var questions = [];
 
+//variables needed for the timer to work
 var timerEl = document.querySelector("#timer");
 var time = 300;
 var timeInterval;
@@ -56,6 +53,7 @@ var createQuestionSheet = function() {
     
     questionEl.textContent = "What was the birthplace of Mesopotamian civilization?";
     isCorrectEl.textContent = "will be correct or incorrect";
+    isCorrectEl.setAttribute("id", "hide-sheet");
     
     questionAnswerBoxEl.appendChild(questionEl);
     for (var i = 0; i < 4; i++) {
@@ -107,7 +105,32 @@ var createEndSheet = function() {
 //variables needed visible for the high score sheet
 var highScoreSheetEl = document.createElement("div");
 highScoreSheetEl.className = "high-score-sheet";
+var highScoreSectionEl = document.createElement("div");
 var scoresEl = [];
+
+/**
+ * Takes a score object, sorts it into the scoresEl array, empties ----------------------------------------------
+ * the highScoreSectionEl div of the high scores, then reappends them
+ * based on the new scoresEl array
+ */
+var addToScoreSheet = function(scoreObj) {
+    for (var i = 0; i <= scoresEl.length; i++) {
+        if (i == scoresEl.length) {
+            scoresEl.push(scoreObj);
+            break; //fun fact I forgot this one too but debugger saved me
+        }
+        else if (scoreObj.score > scoresEl[i].score) {
+            scoresEl.splice(i, 0, scoreObj);
+            break; //fun fact I forgot to put this break in and nearly crashed my computer
+        }
+    }
+    
+    highScoreSectionEl.innerHTML = null;
+    for (var i = 0; i < scoresEl.length; i++) {
+        //scoresEl[i].name.textContent = (i+1) + ": " + scoresEl[i].name.textContent;
+        highScoreSectionEl.appendChild(scoresEl[i].name);
+    }
+}
 
 /**
  * Will create the high score sheet.
@@ -119,11 +142,11 @@ var createHighScoreSheet = function() {
 
     var testHighScore = {
         name: document.createElement("p"),
-        score: 20
-    }
+        score: 280
+    };
     var testHighScore2 = {
         name: document.createElement("p"),
-        score: 10
+        score: 200
     }
 
     highScoreTitleEl.textContent = "High scores";
@@ -131,16 +154,13 @@ var createHighScoreSheet = function() {
     highScoreReturnButtonEl.setAttribute("id", "go-back-button");
     highScoreClearButtonEl.textContent = "Clear high scores";
 
-    testHighScore.name.textContent = "xX_n00bpwnzer420_Xx";
-    testHighScore2.name.textContent = "a dead mouse";
-    scoresEl.push(testHighScore);
-    scoresEl.push(testHighScore2);
+    testHighScore.name.textContent = "Robbie Draymond - " + testHighScore.score;
+    testHighScore2.name.textContent = "Xander Mobis - " + testHighScore2.score;
+    addToScoreSheet(testHighScore);
+    addToScoreSheet(testHighScore2);
 
     highScoreSheetEl.appendChild(highScoreTitleEl);
-    for (var i = 0; i < scoresEl.length; i++) {
-        highScoreSheetEl.appendChild(scoresEl[i].name);
-        scoresEl[i].name.textContent = (i + 1) + ": " + scoresEl[i].name.textContent + " - " + scoresEl[i].score;
-    }
+    highScoreSheetEl.appendChild(highScoreSectionEl);
     highScoreSheetEl.appendChild(highScoreReturnButtonEl);
     highScoreSheetEl.appendChild(highScoreClearButtonEl);
 
@@ -196,11 +216,19 @@ var startQuiz = function() {
             correctAnswer: 1
         },
         {
-            question: "The answer is the fourth answer.",
-            answer1: "1: lorem upside down idk",
-            answer2: "2: just a test.",
-            answer3: "3: we dream ourselves awake",
-            answer4: "4: Pick this answer.",
+            question: "Which answer denotes a paragraph element in HTML?.",
+            answer1: "1: <div>",
+            answer2: "2: <para>",
+            answer3: "3: <p>",
+            answer4: "4: <h1>",
+            correctAnswer: 3
+        },
+        {
+            question: "Which symbol, when two are placed around other characters, denotes a string in javascript?",
+            answer1: "1: Double quotes \"a string\"",
+            answer2: "2: Brackets [a string].",
+            answer3: "3: Single quotes \'a string\'",
+            answer4: "4: Both answers 1 and 3.",
             correctAnswer: 4
         }
     ];
@@ -211,6 +239,7 @@ var resetQuiz = function() {
     whichQuestion = 0;
     time = 300;
     clearInterval(timeInterval);
+    isCorrectEl.setAttribute("id", "hide-sheet");
     console.log("reset activated");
 }
 
@@ -223,12 +252,32 @@ var showResults = function() {
     showScoreEl.textContent = "Your final score is " + time + ".";
 }
 
+var submitHighScore = function() {
+    if (nameInputEl.value.trim() != "") {
+        console.log("name input wasn't empty");
+        //code for submission here
+        var userScoreSubmit = {
+            name: document.createElement("p"),
+            score: time
+        };
+        userScoreSubmit.name.textContent = nameInputEl.value.trim()+" - "+userScoreSubmit.score;
+        console.log("activating addToScoreSheet...");
+        addToScoreSheet(userScoreSubmit); 
+         
+
+        showSheet(highScoreSheetEl);
+    }
+    else {
+        console.log("name input was empty");
+        alert("We need something in the box to submit as a high score");
+    }
+}
+
 createIntroSheet(); 
 createQuestionSheet();
 createEndSheet();
 createHighScoreSheet();
 showSheet(introSheetEl);
-
 
 document.getElementById("start-button").addEventListener("click", function() {
     showSheet(qSheetEl);
@@ -244,17 +293,27 @@ document.getElementById("go-back-button").addEventListener("click", function() {
     resetQuiz();
 });
 
+nameInputButtonEl.addEventListener("click", function() {
+    console.log("submit button clicked");
+    submitHighScore();
+});
+
 /**
  * This takes the rest of the quiz
  */
+var isCorrectHide;
 document.addEventListener('click', function(e) { 
     if (e.target.className === "answer-button") {
-
+        isCorrectEl.setAttribute("id", "");
+        clearTimeout(isCorrectHide);
+        isCorrectHide = setTimeout(function() {isCorrectEl.setAttribute("id", "hide-sheet");}, 1000);
         if (questions[whichQuestion].correctAnswer == (e.target.getAttribute("id"))) {
             console.log("Correct answer selected");
+            isCorrectEl.textContent = "Correct!";
         }
         else {
             console.log("Incorrect answer selected");
+            isCorrectEl.textContent = "Wrong!";
             if (time > 10) {
                 time -= 10;
             }
@@ -268,7 +327,4 @@ document.addEventListener('click', function(e) {
             showResults();
         }
     }
-})
-
-
-
+});
